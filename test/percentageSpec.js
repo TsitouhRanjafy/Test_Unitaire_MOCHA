05b1e91d2a8e0d777a9e68917076bd67f85c046a
RAMIARANJAFY Tsitohaina <ramiaranjafy@kali>
2024-06-22 20:15:14 +0200

    code asynchrone

--- test/percentageSpec.js
+++ test/percentageSpec.js
@@ -1,5 +1,4 @@
 /* ____ INTERFACES BDD
-*  ____ REPORTER SPEC
 */
 // Importation de notre objet
 var Percentage = require("../lib/Percentage");
@@ -7,22 +6,52 @@ var Percentage = require("../lib/Percentage");
 var assert = require("assert");
 // Intialisation du test percentage
 describe('Percentage', function () {
+
     // Intialisation du sous-methode évolution
     describe('#evolution', function () {
+
+        // permet d'indiquer une fonction à executer avant de tout le lancement de tout le test en bas
+        before(function(){
+         
+        })
+
+
         it('should give an evolution', function () {
             // On dois travailler maintenant sur notre objet (importeo le objet)
             //test d'evolution sur 100 et 200
             // on test si l'evolution est à 100
-            assert.default.equal(Percentage.evolution(100, 200), 100);
-            assert.default.equal(Percentage.evolution(1, 1.5), 0);
+            assert.equal(Percentage.evolution(100, 200), 100);
+            assert.equal(Percentage.evolution(1, 1.5), 50);
         });
-        it.only('should handle 0 evolution', function () {
+        it('should handle 0 evolution', function () {
             // test s'il est infinie l'evolution de 0 à 2
             assert.equal(Percentage.evolution(0, 2), Infinity);
         });
         it('should round values', function () {
-            // test avec un arroundissement à 2 d'un evolution 
+            // test avec un arroundissement à 2 d'une evolution 
             assert.equal(Percentage.evolution(30, 124, 2), 313.33);
         });
     });
+
+    // Initialisation de sous-methode wait
+    describe('#wait',function(){
+
+        // test si la fonction wait est definie
+        it.skip('should exit',function(){
+            assert.notEqual(Percentage.wait,undefined);
+        })
+        //
+        it('should wait',function(done){
+            // ici, on fait attendre 1 seconde
+            // une fois 1s écouler on campare le test qui un valeur retournée par le callback , qui était 18
+            Percentage.wait(1000,function(test){
+                assert.equal(test,18);
+                // pour dire que mon test a bien fini de s'executer
+                // ou pour signaler que le test est terminé et que test == 18
+                done();
+            })
+        })
+        // pour qu'il en pending
+        it('should draw unicorns')
+    })
 });
